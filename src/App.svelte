acba44f5a4b01ae526159bc464130466dd4909b7
ChaseMilligan <chasemilligan22@gmail.com>
2020-04-05 19:16:09 -0600

    more mobile

--- src/App.svelte
+++ src/App.svelte
@@ -18,7 +18,6 @@
   }
 
   h1 {
-    font-size: 4em;
     font-weight: 800;
   }
 
@@ -32,7 +31,6 @@
   }
 
   .winner-mask h1 {
-    font-size: 8rem;
     border-bottom: solid 1px #fff;
   }
 </style>
@@ -44,10 +42,10 @@
       <h2 class="uppercase font-comfortaa text-white font-bold">
         Congratulations!
       </h2>
-      <h1 class="uppercase text-white">{winner}</h1>
+      <h1 class="uppercase text-white text-6xl sm:text-8xl">{winner}</h1>
     </div>
   {/if}
-  <h1 class="font-700 font-comfortaa text-red-500">Turny</h1>
+  <h1 class="font-700 font-comfortaa text-red-500 text-6xl">Turny</h1>
   <div class="flex flex-col-reverse xl:flex-row mx-2 md:mx-6 lg:mx-12">
     <div class="mb-4 px-1 w-full xl:w-1/3">
       <PlayerList bind:players />
